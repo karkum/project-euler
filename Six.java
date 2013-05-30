5daee2cb84fed2d2a6657a4b56535793837b6f62
Karthik Kumar <kkumar91@vt.edu>
2013-05-30 03:32:30 -0400

    six

---
+++ Six.java
@@ -0,0 +1,12 @@
+public class Six
+{
+    public static void main(String [] args) {
+        int sumofsquares = 0;
+        int sum = 0;
+        for (int i = 1; i < 101; i++) {
+            sumofsquares += i*i;
+            sum += i;
+        }
+        System.out.println(sum*sum - sumofsquares);
+    }
+}
