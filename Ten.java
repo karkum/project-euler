1026223954b7de98f401c80b300b8a2abaf39f1f
Karthik Kumar <kkumar91@vt.edu>
2013-06-03 13:50:29 -0300

    added comments

--- Ten.java
+++ Ten.java
@@ -1,3 +1,12 @@
+/*
+ * Problem: The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+   Find the sum of all the primes below two million.
+   
+   Answer: 142913828922
+   
+   Algorithm: Sieve of Eratosthenes
+ */
+
 import java.math.BigInteger;
 
 public class Ten {
